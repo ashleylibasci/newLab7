df4fc3dbfb71a3c5503445361cc2571b04ed73dc
Your Name <youremail@yourdomain.com>
2023-06-29 13:36:13 -0500

    update

---
+++ src/newLab7.cpp
@@ -0,0 +1,17 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/ashleylibasci/IoTFolderLabs/newLab7/src/newLab7.ino"
+
+void setup();
+void loop();
+#line 2 "/Users/ashleylibasci/IoTFolderLabs/newLab7/src/newLab7.ino"
+void setup() {
+  
+}
+
+void loop() {
+ 
+}
