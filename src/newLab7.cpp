fb3ff338138a546c65bef0727cd5550eded637fb
Your Name <youremail@yourdomain.com>
2023-06-29 21:09:24 -0500

    update

--- src/newLab7.cpp
+++ src/newLab7.cpp
@@ -4,14 +4,47 @@
 
 #include "Particle.h"
 #line 1 "/Users/ashleylibasci/IoTFolderLabs/newLab7/src/newLab7.ino"
+#include "env.h"
+#include <blynk.h>
+#include "oled-wing-adafruit.h"
 
 void setup();
 void loop();
-#line 2 "/Users/ashleylibasci/IoTFolderLabs/newLab7/src/newLab7.ino"
-void setup() {
-  
+#line 5 "/Users/ashleylibasci/IoTFolderLabs/newLab7/src/newLab7.ino"
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+
+void setup()
+{
+  Blynk.begin(BLYNK_AUTH_TOKEN);
+  pinMode(A4, INPUT);
+
+  display.setup();
+  display.clearDisplay();
+  display.display(); // cause the display to be updated
 }
 
-void loop() {
- 
+void loop()
+{
+  Blynk.run();
+
+  uint64_t reading = analogRead(A4);
+  double voltage = (reading * 3.3) / 4095.0;
+  double celsius = (voltage - 0.5) * 100;
+
+  display.loop();
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(celsius);
+  display.display();
+
+  Blynk.virtualWrite(V0, celsius);
+
+  if (display.pressedA())
+  {
+    Blynk.logEvent("oledButtonPressed");
+  }
 }
--- src/newLab7.ino
+++ src/newLab7.ino
@@ -1,9 +1,41 @@
 #include "env.h"
+#include <blynk.h>
+#include "oled-wing-adafruit.h"
+
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
 
 void setup()
 {
+  Blynk.begin(BLYNK_AUTH_TOKEN);
+  pinMode(A4, INPUT);
+
+  display.setup();
+  display.clearDisplay();
+  display.display(); // cause the display to be updated
 }
 
 void loop()
 {
+  Blynk.run();
+
+  uint64_t reading = analogRead(A4);
+  double voltage = (reading * 3.3) / 4095.0;
+  double celsius = (voltage - 0.5) * 100;
+
+  display.loop();
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(celsius);
+  display.display();
+
+  Blynk.virtualWrite(V0, celsius);
+
+  if (display.pressedA())
+  {
+    Blynk.logEvent("oledButtonPressed");
+  }
 }
